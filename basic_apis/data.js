e2939ade873cad2a01b48a40d8501c1a60db68ee
ABHINAV KUMAR <56204649+abhi9avx@users.noreply.github.com>
2024-01-28 12:02:50 +0530

    Add files via upload

---
+++ basic_apis/data.js
@@ -0,0 +1,7 @@
+const data=[
+    {name : 'Abhinav' , email:'itsabhinav@gmail.com', contact :'9876789876'},
+    {name : 'Abhishek' , email:'itsabhishek@gmail.com', contact :'9876789876'},
+    {name : 'Manas' , email:'itsmanas@gmail.com'},
+    {name : 'Gaurav' , email:'itsgaurav@gmail.com'}
+]
+module.exports= data;
